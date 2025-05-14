0ef98901ed1eef5e1371cf95b50a92e1be83f295
nbarredo <nbarredo@techietalent.net>
2025-05-14 17:30:36 -0300

    removing fed

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,19 @@
 import { defineConfig } from 'vite';
 import singleSpa from 'vite-plugin-single-spa';
-import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   plugins: [
     singleSpa({
       type: 'root',
-      imo:true,
-      imoUi: 'full' ,
+      imo: true,
+      imoUi: 'full',
       importMaps: { 
-        type:'systemjs-importmap',
-        dev:  'src/importMap.dev.json',
-        build:'src/importMap.json'
+        type: 'systemjs-importmap',
+        dev: 'src/importMap.dev.json',
+        build: 'src/importMap.json'
       }
-    }),
-  
-  federation({
-    name: 'root',
-    remotes: {
-      shared_ui: 'https://terrabostmanagestorage.blob.core.windows.net/$web/shared-ui/remoteEntry.js', // Update URL to match your deployment
-    },
-    shared: {
-      react: { 
-        requiredVersion: '^19.0.0',
-        import: false
-      },
-      'react-dom': { 
-        requiredVersion: '^19.0.0',
-        import: false
-      }, 
-      'tailwindcss': {
-        requiredVersion: '^4.0.0',
-        import: false
-      }
-    }
-  })],
+    })
+  ],
   build: {
     target: 'esnext',
     modulePreload: false,
