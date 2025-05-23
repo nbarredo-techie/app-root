16cda8e603aa678eb552ac91dbc97571a659f401
nbarredo <nbarredo@techietalent.net>
2025-05-23 12:44:10 -0300

    adding federation

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const deps = require("./package.json").dependencies;
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "terraboost";
@@ -15,6 +17,30 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
     plugins: [
+      new ModuleFederationPlugin({
+        name: "root_config",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./react": "react",
+          "./react-dom": "react-dom",
+          "./react-dom/client": "react-dom/client",
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+          "react-dom/client": {
+            singleton: true,
+            requiredVersion: deps["react-dom"], // Assumes react-dom version covers client
+          },
+        },
+      }),
       new HtmlWebpackPlugin({
         inject: false,
         template: "src/index.ejs",
